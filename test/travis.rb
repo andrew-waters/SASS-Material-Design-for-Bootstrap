d730b682f3a057324273f68ce849bf85a2c62866
Andrew Waters <andrew@band-x-media.co.uk>
2014-11-14 10:53:47 +0000

    Added Travis

---
+++ test/travis.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+result = `sass assets/material.scss:build/material-bootstrap.css`
+raise result unless $?.to_i == 0
+raise "When compiled the module should output some CSS" unless File.exists?('build/material-bootstrap.css')
+puts "Regular compile worked successfully"
