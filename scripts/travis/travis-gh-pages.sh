5f22ba61fce2de2796b25bb49d72222bedb27afb
Andrew Waters <andrew@band-x-media.co.uk>
2014-11-14 12:42:46 +0000

    Updated travis to include gh-pages script

---
+++ scripts/travis/travis-gh-pages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+  echo -e "Starting to update gh-pages\n"
+
+  #copy data we're interested in to other place
+  cp -R coverage $HOME/demo
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://$GITAUTH@github.com/band-x-media/SASS-Material-Design-for-Bootstrap.git gh-pages > /dev/null
+
+  #go into diractory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/demo/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Done magic with coverage\n"
+
+fi
--- scripts/travis/travis.rb
+++ scripts/travis/travis.rb
@@ -3,4 +3,5 @@ bower = `bower install`
 result = `sass assets/material.scss:build/material-bootstrap.css`
 raise result unless $?.to_i == 0
 raise "When compiled the module should output some CSS" unless File.exists?('build/material-bootstrap.css')
+page = `./scripts/travis-gh-pages.sh`
 puts "Regular compile worked successfully"
