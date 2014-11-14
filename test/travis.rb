c929ffee63819bbd9273c65e96873f01d8cae3c1
Andrew Waters <andrew@band-x-media.co.uk>
2014-11-14 11:00:23 +0000

    Added testing scripts

--- test/travis.rb
+++ test/travis.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+bower = `bower install`
 result = `sass assets/material.scss:build/material-bootstrap.css`
 raise result unless $?.to_i == 0
 raise "When compiled the module should output some CSS" unless File.exists?('build/material-bootstrap.css')
