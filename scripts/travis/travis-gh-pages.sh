82c9ee06ab130d6b66a50b6d79f3a8eb23ae52bd
Andrew Waters <andrew@band-x-media.co.uk>
2014-11-14 13:16:46 +0000

    Updated demo location

--- scripts/travis/travis-gh-pages.sh
+++ scripts/travis/travis-gh-pages.sh
@@ -14,7 +14,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
-  cp -Rf $HOME/demo/* .
+  cp -Rf ./demo/* .
 
   #add, commit and push files
   git add -f .
