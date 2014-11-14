a3bf34a6db1f2f9f29755e84dfa55bea096afe1f
Andrew Waters <andrew@band-x-media.co.uk>
2014-11-14 13:31:51 +0000

    build test

--- scripts/travis/travis-gh-pages.sh
+++ scripts/travis/travis-gh-pages.sh
@@ -4,6 +4,9 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   echo -e "Starting to update gh-pages\n"
 
+  #copy data we're interested in to other place
+  cp -R ./demo $HOME/demo
+
   #go to home and setup git
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
@@ -13,13 +16,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git clone --quiet --branch=gh-pages https://$GITAUTH@github.com/band-x-media/SASS-Material-Design-for-Bootstrap.git gh-pages > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
-  cp -Rf ./demo/* .
+  cd gh-pages
+  cp -Rf $HOME/demo/* .
 
   #add, commit and push files
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Updated gh-pages\n"
+  echo -e "Done magic with coverage\n"
 
 fi
