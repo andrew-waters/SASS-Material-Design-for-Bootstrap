87de48152a51f8d1bcd6ce884b198b6ad5b214f7
Andrew Waters <andrew@band-x-media.co.uk>
2014-11-15 09:15:59 +0000

    Updated demo site

--- pages/js/colours.js
+++ pages/js/colours.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
 
 	//	build the background colours
 		$container = $('<div />', {
-			class: 'background-color-container col-lg-4 col-md-4 col-sm-6 col-xs-6',
+			class: 'background-color-container col-lg-3 col-md-4 col-sm-12 col-xs-12',
 			id: 'background-color-' + color
 		}).appendTo('#background-colors');
 
