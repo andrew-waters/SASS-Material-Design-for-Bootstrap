9808ce184cb621886177f0d387cbb1625a7418f3
Andrew Waters <andrew@band-x-media.co.uk>
2014-11-16 12:46:32 +0000

    Updated build script to copy fonts as well

--- scripts/travis/publish-gh-pages.sh
+++ scripts/travis/publish-gh-pages.sh
@@ -2,32 +2,35 @@
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-  echo -e "Starting to update gh-pages\n"
+	echo -e "Starting to update gh-pages\n"
 
-  #copy data we're interested in to other place
-  cp -R ./pages $HOME/pages
+#	copy pages to the home directory
+	cp -R ./pages $HOME/pages
 
-  #go to home and setup git
-  cd $HOME
-  git config --global user.email "andrew@band-x-media.co.uk"
-  git config --global user.name "Andrew Waters"
+#	copy bootstrap fonts
+	cp -R ./bower_components/bootstrap-sass-official/assets/fonts $HOME/pages/fonts
 
-  #using token clone gh-pages branch
-  curl -H 'Authorization: token $GITAUTH' https://github.com/band-x-media/SASS-Material-Design-for-Bootstrap.git
-  git clone --quiet --branch=gh-pages https://andrew-waters:$GITAUTH@github.com/band-x-media/SASS-Material-Design-for-Bootstrap.git gh-pages > /dev/null
+#	go to home and setup git
+	cd $HOME
+	git config --global user.email "andrew@band-x-media.co.uk"
+	git config --global user.name "Andrew Waters"
 
-  #go into diractory and copy data we're interested in to that directory
-  cd gh-pages
-  git remote rm origin
-  git remote add origin https://andrew-waters:$GITAUTH@github.com/band-x-media/SASS-Material-Design-for-Bootstrap.git
-  
-  cp -Rf $HOME/pages/* .
+#	using token clone gh-pages branch
+	curl -H 'Authorization: token $GITAUTH' https://github.com/band-x-media/SASS-Material-Design-for-Bootstrap.git
+	git clone --quiet --branch=gh-pages https://andrew-waters:$GITAUTH@github.com/band-x-media/SASS-Material-Design-for-Bootstrap.git gh-pages > /dev/null
 
-  #add, commit and push files
-  git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-  git push -fq origin gh-pages > /dev/null
+#	go into diractory and copy data we're interested in to that directory
+	cd gh-pages
+	git remote rm origin
+	git remote add origin https://andrew-waters:$GITAUTH@github.com/band-x-media/SASS-Material-Design-for-Bootstrap.git
 
-  echo -e "Changes published to gh-pages branch\n"
+	cp -Rf $HOME/pages/* .
+
+#	add, commit and push files
+	git add -f .
+	git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+	git push -fq origin gh-pages > /dev/null
+
+	echo -e "Changes published to gh-pages branch\n"
 
 fi
