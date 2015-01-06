11c8cb03f3a2b3c8e8d0e5089332a3a3ca5489a1
Andrew Waters <andrew@band-x-media.co.uk>
2015-01-06 22:12:52 +0000

    Removed scroll from mobile

--- docs/_site/assets/js/main.js
+++ docs/_site/assets/js/main.js
@@ -2,6 +2,9 @@
 $(document).ready(function() {
 	skrollr.init({
 		smoothScrolling: true,
-		smoothScrollingDuration: 1
+		smoothScrollingDuration: 1,
+		mobileCheck: function() {
+			return false;
+		}
 	});
 });
