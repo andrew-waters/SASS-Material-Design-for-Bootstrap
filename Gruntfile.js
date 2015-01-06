8591c2c8bede48a38da79dcfa1da16a5f7a52690
Andrew Waters <andrew@band-x-media.co.uk>
2015-01-06 16:55:40 +0000

    Added JS build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
+
 module.exports = function(grunt) {
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
@@ -12,9 +14,11 @@ module.exports = function(grunt) {
 			}
 		},
 		concat: {
-			dist: {
-				src: '_docs/assets/css/material-bootstrap.css',
-				dest: 'dist/material-bootstrap.css'
+			docs: {
+				files: {
+					'_docs/assets/css/material-bootstrap.min.css' : 'dist/material-bootstrap.min.css',
+					'_docs/assets/js/material-bootstrap.min.js' : 'dist/material-bootstrap.min.js'
+				}
 			}
 		},
 		cssmin: {
@@ -26,16 +30,44 @@ module.exports = function(grunt) {
 				dest: 'dist/material-bootstrap.min.css'
 			}
 		},
+		uglify: {
+			options: {
+				mangle: true
+			},
+			dist: {
+				files: {
+					'dist/material-bootstrap.min.js': [
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/affix.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/alert.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/button.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/carousel.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/collapse.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/modal.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tooltip.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/popover.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/scrollspy.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tab.js',
+						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/transition.js',
+						'src/javascripts/sidebar.js',
+						'src/javascripts/smooth-scrolling.js'
+					]
+				}
+			}
+		},
 		watch: {
 			dist: {
-				files: '**/*.scss',
-				tasks: ['sass:dist', 'concat:dist', 'cssmin:dist']
+				files: ['**/*.scss', '**/*.js'],
+				tasks: ['sass:dist', 'cssmin:dist', 'uglify:dist', 'concat:docs']
 			}
 		}
 	});
+
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
- 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.registerTask('default', ['watch:dist']);
+
 }
