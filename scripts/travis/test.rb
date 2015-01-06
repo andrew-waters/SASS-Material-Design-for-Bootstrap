fab8a62221aa73ac3b45b43d30025707ee367cbc
Andrew Waters <andrew@band-x-media.co.uk>
2015-01-06 16:03:33 +0000

    Updated test path

--- scripts/travis/test.rb
+++ scripts/travis/test.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 bower = `bower install`
-result = `sass assets/material.scss:_docs/assets/css/material-bootstrap.css`
+result = `sass src/material.scss:_docs/assets/css/material-bootstrap.css`
 raise result unless $?.to_i == 0
 raise "When compiled the module should output some CSS" unless File.exists?('_docs/assets/css/material-bootstrap.css')
 puts "Regular compile worked successfully"
