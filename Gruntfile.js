3d2a3619cae94ec0ffcc294e52ffb76ee84e6937
Andrew Waters <andrew@band-x-media.co.uk>
2015-06-13 17:05:07 +0100

    Added Material Design Floating Labels for Inputs

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,6 +45,8 @@ module.exports = function(grunt) {
 						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/scrollspy.js',
 						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/tab.js',
 						'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/transition.js',
+						'bower_components/inputs/assets/javascripts/inputs.js',
+						'src/javascripts/material-form.js',
 						'src/javascripts/app-bar.js',
 						'src/javascripts/navigation-drawer.js',
 						'src/javascripts/smooth-scrolling.js'
@@ -56,13 +58,6 @@ module.exports = function(grunt) {
 		copy: {
 			docs: {
 				files: [
-					{
-						expand: true,
-						flatten: true,
-						src: ['src/fonts/material/*'],
-						dest: 'docs/assets/fonts/material',
-						filter: 'isFile'
-					},
 					{
 						expand: true,
 						flatten: true,
---
+++ src/javascripts/material-form.js
@@ -0,0 +1,76 @@
+$(function(){
+
+	$.fn.hasAttr = function(attribute){
+		var attr = this.attr(attribute);
+		if (typeof attr !== typeof undefined && attr !== false) 
+		   return true;
+		return false;
+	}
+
+	$.fn.materialInput = function() {
+
+	    this.find('input, textarea').each(function(i){
+
+	    	if(isValidType($(this))) {
+
+	    		var name = $(this).attr('name');
+
+	    		$(this).attr('id', name);
+
+		    	var $wrap = $(this).wrap("<div class='material-input'></div>").parent();
+
+		    	var tagName = $(this).prop("tagName").toLowerCase();
+
+		    	$wrap.addClass(tagName);
+		    	
+		    	var placeholder = $(this).attr('placeholder');
+
+		    	if(placeholder){
+		    		$wrap.append("<label for='" + name + "'>" + placeholder + "</label>");
+		    		$(this).removeAttr('placeholder');
+		    	}
+
+		    	addFilled($(this));
+
+	    	}
+
+	    });
+
+	    this.find('input, textarea').on('blur', function(){
+		    if(isValidType($(this)))
+		    	addFilled($(this))
+	    });
+
+	    // Radio
+
+	    function isValidType($el){
+	    	var type = $el.attr('type');
+	    	return (type = 'hidden' && type != 'submit' && type != 'checkbox' && type != 'radio' && type != 'file' ? 1 : 0);
+	    }
+
+	    function isType($el, type){
+	    	var el_type = $el.attr('type');
+	    	return (el_type == type);
+	    }
+
+	    function addFilled($el){
+	    	if($el.val())
+	    		$el.addClass('filled');
+	    	else
+	    		$el.removeClass('filled');
+	    }
+
+		$('.material-select > input').on('change', function(){
+			var changed_id = $(this).attr('id');
+			$('.material-select > input').each(function(){
+				var this_id = $(this).attr('id');
+				if(changed_id != this_id)
+					$(this).prop('checked', false);
+			});
+		});
+
+	};
+
+	$('.material').materialInput();
+
+});
