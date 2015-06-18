c67230dab1ba2e19cfcfb9e8268eeafc75d941de
Andrew Waters <andrew@band-x-media.co.uk>
2015-06-18 17:04:51 +0100

    Added Cards (initial support)

--- src/javascripts/_init.js
+++ src/javascripts/_init.js
@@ -7,5 +7,13 @@ $(document).ready(function() {
 		$(this).validate();
 	});
 
+	$('.card').each(function() {
+
+		var first = $(this).children().first();
+
+	//	this adds a top border radius to media element images that are at the top of the card
+		if(first.hasClass('tile')) first.addClass('top');
+
+	});
 
 });
