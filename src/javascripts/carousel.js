8bbac2d679c262b360b249c7d752ef1c8f0bca4e
Andrew Waters <andrew@band-x-media.co.uk>
2015-06-18 21:57:37 +0100

    Updated Responsiveness of Cards in Carousel

--- src/javascripts/carousel.js
+++ src/javascripts/carousel.js
@@ -1,40 +1,42 @@
+
 $(document).ready(function() {
 
-$('.your-class').slick({
+	$('.your-class').slick({
 
-/*
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-*/
-  dots: true,
-  arrows: true,
-//  appendArrows: $('.your-class-arrows'),
-  prevArrow: $('.slick-prev'),
-  nextArrow: $('.slick-next'),
-  centerMode: false,
-  variableWidth: false,
-  infinite: false,
-  initialSlide: 0,
-  slidesToShow: 3,
-  rows: 1,
-  slidesToScroll: 2,
-  responsive: [
-  {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 2
-      }
-	 },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1
-      }
-    }
-  ]
+		dots: true,
+		arrows: true,
+		prevArrow: $('.slick-prev'),
+		nextArrow: $('.slick-next'),
+		centerMode: false,
+		variableWidth: false,
+		infinite: false,
+		initialSlide: 0,
+		slidesToShow: 4,
+		rows: 1,
+		slidesToScroll: 2,
 
-});
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+
+	});
 
 });
 
