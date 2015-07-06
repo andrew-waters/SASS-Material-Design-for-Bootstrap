693118db28fa6533147ff31c68ade042b43b8255
Andrew Waters <andrew@band-x-media.co.uk>
2015-07-06 13:32:00 +0100

    Added correct offsets for smooth scrolling

--- src/javascripts/smooth-scrolling.js
+++ src/javascripts/smooth-scrolling.js
@@ -8,9 +8,19 @@ $(document).ready(function() {
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 				var target = $(this.hash);
 				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+			//	calculate the top offset
+				var topOffset = target.offset().top - 20;
+				
+				var appBar = $('nav.app-bar');
+				if(appBar) {
+					topOffset -= appBar.height();
+					if(appBar.hasClass("has-tool-bar")) topOffset -= 60;
+				}
+
 				if (target.length) {
 					$('html,body').animate({
-						scrollTop: target.offset().top
+						scrollTop: topOffset
 					}, 1000);
 					return false;
 				}
