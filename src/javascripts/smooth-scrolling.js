7bf614e7e5548c29190b76fe09f14d3de9287fbd
Andrew Waters <andrew@band-x-media.co.uk>
2015-07-06 13:50:55 +0100

    Added data-attr to customise smooth scrolling

--- src/javascripts/smooth-scrolling.js
+++ src/javascripts/smooth-scrolling.js
@@ -4,28 +4,37 @@
 $(document).ready(function() {
 
 	$(function() {
+
 		$('a[href*=#]:not([href=#])').click(function() {
+
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
 				var target = $(this.hash);
 				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
 			//	calculate the top offset
-				var topOffset = target.offset().top - 20;
-				
+				var topOffset = target.offset().top - 13;				
 				var appBar = $('nav.app-bar');
 				if(appBar) {
 					topOffset -= appBar.height();
 					if(appBar.hasClass("has-tool-bar")) topOffset -= 60;
 				}
 
-				if (target.length) {
-					$('html,body').animate({
-						scrollTop: topOffset
-					}, 1000);
+				var duration = 1000;
+				if($(this).attr("data-scroll-duration")) duration = $(this).attr("data-scroll-duration");
+
+				var easing = "easeInOutQuint";
+				if($(this).attr("data-scroll-easing")) easing = $(this).attr("data-scroll-easing");
+
+				if(target.length) {
+					$('html, body').velocity('scroll', { duration: duration, offset: topOffset, easing: easing });
 					return false;
 				}
+
 			}
+
 		});
+
 	});
 
 });
